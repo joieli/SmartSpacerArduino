1123b1e4fdf0c1c0ac719c0f7aaf69006540357a
Joie Li <g246li@uwaterloo.ca>
2025-02-20 16:08:54 -0500

    changed var name

--- JoieStuff/rgb_led_test/rgb_led_test.ino
+++ JoieStuff/rgb_led_test/rgb_led_test.ino
@@ -3,7 +3,7 @@ const int enablePin = A2; //shiftRegister pin 13 (orange wire)
 const int latchPin = A3; //shiftRegister pin 11(purple wire)
 const int clockPin = A4; //shiftRegister pin 12 (blue wire)
 
-uint16_t prevState = 0b0000000000000000;
+uint16_t prevLightState = 0b0000000000000000;
 
 
 // Number of LEDs (RGB LEDs)
@@ -75,43 +75,43 @@ void setColor(String color, int ledIndex) {
   }
 
   // Send color data to the correct LED
-  setLEDColor(ledIndex, red, green, blue, prevState);
-  Serial.println(prevState, BIN);
+  setLEDColor(ledIndex, red, green, blue, prevLightState);
+  Serial.println(prevLightState, BIN);
 }
 
 // Function to send the color to a specific LED
-void setLEDColor(int ledIndex, byte red, byte green, byte blue, uint16_t &prevState) {
-  uint16_t newState;
+void setLEDColor(int ledIndex, byte red, byte green, byte blue, uint16_t &prevLightState) {
+  uint16_t newLightState;
   if(red == 1){
-    newState = bitSet(prevState, ledIndex*3);
+    newLightState = bitSet(prevLightState, ledIndex*3);
   }
   else if(red == 0){
-    newState = bitClear(prevState, ledIndex*3);
+    newLightState = bitClear(prevLightState, ledIndex*3);
   }
 
   if(green == 1){
-    newState = bitSet(prevState, ledIndex*3+2);
+    newLightState = bitSet(prevLightState, ledIndex*3+2);
   }
   else if(green == 0){
-    newState = bitClear(prevState, ledIndex*3+2);
+    newLightState = bitClear(prevLightState, ledIndex*3+2);
   }
 
   if(blue == 1){
-    newState = bitSet(prevState, ledIndex*3+1);
+    newLightState = bitSet(prevLightState, ledIndex*3+1);
   }
   else if(blue == 0){
-    newState = bitClear(prevState, ledIndex*3+1);
+    newLightState = bitClear(prevLightState, ledIndex*3+1);
   }
 
 
   digitalWrite(enablePin, LOW);
 
   digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, newState >> 8);
-  shiftOut(dataPin, clockPin, MSBFIRST, newState);
+  shiftOut(dataPin, clockPin, MSBFIRST, newLightState >> 8);
+  shiftOut(dataPin, clockPin, MSBFIRST, newLightState);
   digitalWrite(latchPin, HIGH);
 
-  prevState = newState;
+  prevLightState = newLightState;
 }
 
 void allLEDsOff(){
