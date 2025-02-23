481984afe37ade2cefdd97a2719cec2064a6c15a
Joie Li <g246li@uwaterloo.ca>
2025-02-23 16:29:54 -0500

    can now receive and store config data via bluetooth

--- JoieStuff/bluetooth_test/bluetooth_test.ino
+++ JoieStuff/bluetooth_test/bluetooth_test.ino
@@ -2,6 +2,7 @@
 #include <SD.h>
 #include <Arduino.h>
 #include <SPI.h>
+#include <ArduinoJson.h> //ArduinoJson by Benoit Blanchon, documentation: https://arduinojson.org/v7/
 #include "Adafruit_BLE.h"
 #include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
@@ -19,8 +20,33 @@ Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_
 const int chipSelect = 10;
 File root;
 String fileName;
+String configFileName = "CONFIG.TXT";
+String jsonDataBT = "";
 
 bool endOfFile = false;
+bool hasConfig = false;
+
+class Config{
+  public:
+    String userID;
+    String spacerUDI;
+    bool PEFRActive;
+    int baseline;
+
+    void print(){
+      Serial.println(F("{"));
+      Serial.print(F("  userID: "));
+      Serial.println(userID);
+      Serial.print(F("  spacerUDI: "));
+      Serial.println(spacerUDI);
+      Serial.print(F("  PEFRActive: "));
+      Serial.println(PEFRActive);
+      Serial.print(F("  baseline: "));
+      Serial.println(baseline);
+      Serial.println(F("}"));
+    }
+};
+Config config; //initalizing a config object
 
 void setup() {
   Serial.begin(115200);
@@ -48,8 +74,44 @@ void setup() {
   }
   Serial.println("initialization done.");
 
-  Serial.println("LIST OF DOCUMENTS=========");
+  //setting up the config data
+  Serial.println();
+  if (SD.exists(configFileName)) {
+    Serial.println(F("CONFIG.TXT exists, printing contents..."));
 
+    File configFile = SD.open(configFileName);
+    if (!configFile) {
+      Serial.println(F("Failed to open CONFIG.TXT"));
+    }
+    
+    // Read the JSON data from the file
+    String jsonData = "";
+    while (configFile.available()) {
+      jsonData += (char)configFile.read();
+    }
+    configFile.close();
+
+    // Create a JSON document to parse the data
+    StaticJsonDocument<200> doc;
+    
+    // Deserialize the JSON data
+    DeserializationError error = deserializeJson(doc, jsonData);
+    if (error) {
+      Serial.print("Failed to parse JSON: ");
+      Serial.println(error.f_str());
+      return;
+    }
+
+    config.userID = doc["userID"].as<String>();
+    config.spacerUDI = doc["spacerUDI"].as<String>();
+    config.PEFRActive = doc["PEFRActive"].as<bool>();
+    config.baseline = doc["baseline"].as<int>();
+    config.print();
+
+    hasConfig = true;
+  }
+
+  Serial.println("LIST OF DOCUMENTS=========");
   root = SD.open("/");
 
   printDirectory(root, 0);
@@ -64,14 +126,21 @@ void loop() {
   if(Serial.available()){
     fileName = Serial.readStringUntil('\n');
 
-    if(ble.isConnected() && endOfFile == false){
-      Serial.println("SENDING FOLLOWING CONTENTS OF" + fileName + "================================");
-      sendContentsBT(fileName); //ToDo change me
-      Serial.println("END OF DOCUMENT=========================================");
-      endOfFile = true;
-    } 
+    if(ble.isConnected()){
+      Serial.println(F("BLUETOOTH CONNECTED"));
+      if(endOfFile==false){
+        Serial.println(F("SENDING FOLLOWING CONTENTS OF FOLLOWING FILES: "));
+        sendContentsBT(fileName);
+        endOfFile = true;
+      }
+    }  
   }
 
+  if(ble.isConnected()){
+    Serial.println(F("BLUETOOTH CONNECTED"));
+    receiveContentsBT();
+  }
+  delay(500);
 
 }
 
@@ -123,7 +192,6 @@ void sendContentsBT(String fileName)
         char character = (char)file.read();
         buffer += character;
         if(character==10){ //ASCII code for new line
-          Serial.print(buffer);
           ble.print(buffer);
           buffer = "";
         }
@@ -133,6 +201,8 @@ void sendContentsBT(String fileName)
     ble.print(fileName);
     ble.println(F("***"));
     file.close();
+    Serial.print(F("  "));
+    Serial.println(fileName);
   }
   else{
     Serial.print(F("    error opening "));
@@ -140,4 +210,88 @@ void sendContentsBT(String fileName)
   }
 }
 
+void receiveContentsBT(){
+  while(ble.available()){
+    char character = (char)ble.read();
+    jsonDataBT += character;
+    if(character == 125) {
+      Serial.println(F("Recieved following JSON from BT:"));
+      Serial.println(jsonDataBT);
+      Serial.println();
+      // Create a JSON document to parse the data
+      StaticJsonDocument<200> doc;
+      
+      // Deserialize the JSON data
+      DeserializationError error = deserializeJson(doc, jsonDataBT);
+      if (error) {
+        Serial.print("Failed to parse JSON: ");
+        Serial.println(error.f_str());
+        return;
+      }
+
+      if(config.spacerUDI == doc["spacerUDI"].as<String>()){
+        config.userID = doc["userID"].as<String>();
+        config.PEFRActive = doc["PEFRActive"].as<bool>();
+        config.baseline = doc["baseline"].as<int>();
+
+        rewriteConfigFile(configFileName, config);
+      }
+      jsonDataBT = ""; //reset jsonData
+    }
+  }
+}
+
+void rewriteConfigFile(String filename, Config config){
+  JsonDocument doc;
+  JsonObject root= doc.to<JsonObject>();
+
+  // Set the values in the document
+  root["userID"] = config.userID;
+  root["spacerUDI"] = config.spacerUDI;
+  root["PEFRActive"] = config.PEFRActive;
+  root["baseline"] = config.baseline;
+  
+  //remove exisitng config file to rewrite
+  SD.remove(filename);
+  
+  // Open file for writing
+  File file = SD.open(filename, FILE_WRITE);
+  if (!file) {
+    Serial.print(F("Failed to create file: "));
+    Serial.println(filename);
+    return;
+  }
+
+  // Serialize JSON to file
+  if (serializeJsonPretty(root, file) == 0) {
+    Serial.print(F("  Failed to write to "));
+    Serial.println(filename);
+  }
+  else{
+    Serial.print(F("   Updated "));
+    Serial.println(filename);
+  }
+  file.println();
+
+  // Close the file
+  file.close();
+}
+
+void printContents(String fileName){
+  File file = SD.open(fileName);
+  if (file) {
+    Serial.print(F("BEGINNING OF "));
+    Serial.print(fileName);
+    Serial.println(F("========================="));
+    while (file.available()) {
+      Serial.write(file.read());
+    }
+  file.close();
+  Serial.println(F("END OF DOCUMENT========================="));
+  }
+  else{
+    Serial.print(F("    error opening: "));
+    Serial.println(fileName);
+  }
+}
 
